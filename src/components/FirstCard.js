c83a31ca5da411e5bbce9d2b27a20453dce22dd3
Alexander Gochev <124374696+alexander9856@users.noreply.github.com>
2023-08-21 11:50:42 +0300

    fix: remove from localstorage when you click start button

--- src/components/FirstCard.js
+++ src/components/FirstCard.js
@@ -3,11 +3,13 @@ export const FirstCard = () => {
     return (
         <Flex
             direction='column'
-            p="2.875rem 3.6875rem 3.3125rem 3.75rem"
+            p={["2.875rem 3.6875rem 3.3125rem 3.75rem","2.875rem 3.6875rem 3.3125rem 3.75rem","2.875rem 3.6875rem 3.3125rem 3.75rem","2.875rem 3.6875rem 3.3125rem 3.75rem","2.875rem 3.6875rem 3.3125rem 3.75rem","2.9947916666666665vw 3.8411458333333335vw 3.4505208333333335vw 3.90625vw"]}
             alignItems='center'
             backgroundColor='#EEF7FB'
             borderRadius='0.5rem'
             maxW={['19rem','21.785rem']}
+            minW='349px'
+            minH='430px'
             maxH='100%'
             mb='2.8rem'
         >
